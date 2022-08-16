b70b0520dbebd6f0cce8566e14606d05c39ccf0c
Tuğçe Kaman <tugcekaman43@gmail.com>
2022-08-16 11:27:03 +0300

    Created Unity Project

---
+++ Assets/LevelController.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelController : MonoBehaviour
+{
+    private Enemy[] _enemies;
+    private static int _nextLevelIndex = 1;
+
+    private void OnEnable()
+    {
+        _enemies = FindObjectsOfType<Enemy>();
+    }
+    void Update()
+    {
+        foreach(Enemy enemy in _enemies)
+        {
+            if(enemy != null)
+            {
+                return;
+            }
+        }
+
+        Debug.Log("You killed all enemies");
+        _nextLevelIndex++;
+        string nextLevelName = "Level" + _nextLevelIndex;
+        SceneManager.LoadScene(nextLevelName);
+    }
+}
